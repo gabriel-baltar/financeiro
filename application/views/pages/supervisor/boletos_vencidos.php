dd32f72b2cbba7cc200108136727b5922d71f924
jonatas-gama <jonatasgama@rssolucoesconsultoria.com>
2019-09-09 10:30:36 -0300

    adicionado a função de buscar por data

--- application/views/pages/supervisor/boletos_vencidos.php
+++ application/views/pages/supervisor/boletos_vencidos.php
@@ -4,21 +4,28 @@
 			Boletos Vencidos
 		</h3>
 		<div class="container">
-		<form>
+		
+		<form method="post" class="col-8 offset-2" action="<?=base_url("supervisor/procuraBoletosVencidos");?>">
 			<h3 class="text-center mt-5">Buscar por Data</h3>
 			<div class="row mb-5 mt-5">
-				<div class="col ml-5">
+				<div class="col-4">
 					<label for="inputInicio">Data Início</label>
-					<input type="text" class="form-control col-md-9" placeholder="Data Início">
+					<input type="date" class="form-control" name="inicio" placeholder="Data Início">
 				</div>
-				<div class="col">
+				<div class="col-4">
 					<label for="inputFim">Data Fim</label>
-					<input type="text" class="form-control col-md-9" placeholder="Data Fim">
+					<input type="date" class="form-control" name="fim" placeholder="Data Fim">
 				</div>
+				<div class="col-4">
+					<label>&nbsp</label>
+					<button type="submit" class="form-control btn btn-success">Buscar</button>	
+				</div>		
 			</div>
+
 		</form>
 
 			<div class="row">
+			<?php if(isset($boletos)){ ;?>
 				<table class="table table-hover row-12">
 					<thead>
 						<tr>
@@ -30,7 +37,8 @@
 						</tr>
 					</thead>
 					<tbody>
-					<?php foreach($boletos as $b){; ?>
+
+						<?php foreach($boletos as $b){; ?>
 						<tr>
 							<td><?=$b->codigo_de_barras;?></td>
 							<td><?=$b->beneficiario;?></td>
@@ -38,17 +46,9 @@
 							<td><?=$b->segmento;?></td>
 							<td><?=$b->valor;?></td>
 						</tr>
-					<?php };?>
+						<?php };?>
 					</tbody>
 				</table>
+			<?php } ;?>
 			</div>
-			<nav aria-label="Navegação de página exemplo" class="d-flex justify-content-center">
-				<ul class="pagination">
-					<li class="page-item"><a class="page-link text-dark" href="#">Anterior</a></li>
-					<li class="page-item"><a class="page-link text-dark" href="#">1</a></li>
-					<li class="page-item"><a class="page-link text-dark" href="#">2</a></li>
-					<li class="page-item"><a class="page-link text-dark" href="#">3</a></li>
-					<li class="page-item"><a class="page-link text-dark" href="#">Próximo</a></li>
-				</ul>
-			</nav>
 		</div>
